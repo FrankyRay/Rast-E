f23223df88b1080e714663358a5ca168e168d77f
FrankyRay <frankyraymarcellsinaga@gmail.com>
2024-09-18 11:35:35 +0700

    feat: ability add new shop

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from uuid import uuid4
 
 class Shop(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render, redirect
+from main.forms import ShopForm
+from main.models import Shop
 
 def show_main(request):
     context = {
             'shop': 'Rast-E',
             'name': 'Franky Raymarcell Sinaga',
             'class': 'PBP F',
+            'shops': Shop.objects.all()
     }
 
     return render(request, "main.html", context)
+
+def create_shop(request):
+    form = ShopForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = { "form": form }
+    return render(request, "create_shop.html", context)
+
+def show_xml(request):
+    data = Shop.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_xml_by_id(request, id):
+    data = Shop.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Shop.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = Shop.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
